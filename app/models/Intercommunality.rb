bd20b498eff69516719d43572ba76344402ce848
renaud gagnon <kisscoding@gmail.com>
2019-09-09 23:37:25 +0200

    refactor

--- app/models/Intercommunality.rb
+++ app/models/Intercommunality.rb
@@ -4,16 +4,14 @@ class Intercommunality < ApplicationRecord
 
   has_many :communes
 
-  validates_presence_of :name, :form
-  validates :siren, presence: true, format: { with: /\A[0-9]{9}\z/ }
-  validates_uniqueness_of :siren, case_sensitive: false
-  validates :form, inclusion: { in: %w(ca cu cc met) }
+  validates_presence_of :name 
+  validates :siren, presence: true, uniqueness: true, case_sensitive: false, format: { with: /\A[0-9]{9}\z/ }
+  validates :form, presence: true, inclusion: { in: %w(ca cu cc met) }
 
   before_save :compute_slug
 
-
   def communes_hash
-    communes.map{ |c| [c.code_insee, c.name] }.to_h
+    communes.pluck(:code_insee, :name).to_h
   end
 
   def population
